6a5f932e79423d433e6bd379137e8f7b3f000016
Esra Ceren <s230446022@stu.thk.edu.tr>
2025-10-19 13:50:12 +0300

    init commit

---
+++ creating object/src/example.java
@@ -0,0 +1,19 @@
+public class example {
+    public static void main(String[]args){
+        int sayi=35;
+                if(sayi>30){
+                    System.out.println("sayı 30 değerinden büyüktür");
+                }
+                int sayi2=3;
+                if(sayi2>5){
+                    System.out.println("sayı 5 değerinden büyüktür");
+                }
+                else if(sayi2==5){
+                    System.out.println("sayı2 5 e eşittir");
+                }
+                else{
+                    System.out.println("sayı2 5 ten küçüktür");
+                }
+
+    }
+}
