7bf4bca3defa842e3796da102e3976609998d537
Esra Ceren <s230446022@stu.thk.edu.tr>
2025-10-19 14:49:12 +0300

    init commit

---
+++ creating object/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    car car1= new car();
+    car1.model=2005;
+    car1.brand="mercedes";
+    car1.color="white";
+    car1.displayInfo();
+    Engine engine1 =new Engine();
+    engine1.horsePower=74775;
+    engine1.showPower();
+
+
+}
