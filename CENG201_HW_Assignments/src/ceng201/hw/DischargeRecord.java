241ae3b24fe4afc386b0c79924ff5e5ec6728618
Esra Ceren <s230446022@stu.thk.edu.tr>
2025-12-27 17:53:59 +0300

    init commit final

--- CENG201_HW_Assignments/src/ceng201/hw/DischargeRecord.java
+++ CENG201_HW_Assignments/src/ceng201/hw/DischargeRecord.java
@@ -1,8 +1,8 @@
 package ceng201.hw;
 
 public class DischargeRecord {
-    int patientId;
-    long dischargeTime;
+   public int patientId;
+   public long dischargeTime;
 //Initialize discharge record with patient ID and current time
     public DischargeRecord(int patientId){
         this.patientId=patientId;
